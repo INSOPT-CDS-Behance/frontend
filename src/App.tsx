dc004e7aaf2e8ab7b4fd70347ed206960d5886b0
seohee0112 <ghdtj7@naver.com>
2022-11-08 18:48:22 +0900

    fix : 전역 스타일 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { ThemeProvider } from 'styled-components';
+
 import Router from './components/common/Router';
+import GlobalStyle from './styles/globalStyle';
+import theme from './styles/theme';
 
-const App = () => {
+function App() {
   return (
-    <div>
-      <Router />
-    </div>
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Router />
+      </ThemeProvider>
+    </>
   );
-};
-
+}
 export default App;
