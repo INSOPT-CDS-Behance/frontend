62c8b0f9da1bed79032dfaa82de9d006ffef1b28
seohee0112 <ghdtj7@naver.com>
2022-11-14 23:35:37 +0900

    feat : 프로젝트, 프로젝트 둘러보기 검색 UI 구현

--- src/components/Home/Header.tsx
+++ src/components/Home/Header.tsx
@@ -1,8 +1,21 @@
 import styled from 'styled-components';
 
-import { ICSearch } from '../../asset/icon';
+import { ICDropDown, ICSearch } from '../../asset/icon';
 const Header = () => {
-  return <StHeaderWrapper></StHeaderWrapper>;
+  return (
+    <StHeaderWrapper>
+      <form>
+        <StButton>
+          <span>프로젝트</span>
+          <ICDropDown fill="white" />
+        </StButton>
+        <StInputWrapper>
+          <ICSearch width="24" height="24" />
+          <input placeholder="Behance 추천 프로젝트 둘러보기" />
+        </StInputWrapper>
+      </form>
+    </StHeaderWrapper>
+  );
 };
 
 export default Header;
@@ -14,4 +27,48 @@ const StHeaderWrapper = styled.header`
   width: 100%;
   height: rem;
   padding: 0 1.875rem;
+
+  & > form {
+    display: flex;
+
+    width: 100%;
+  }
+`;
+const StInputWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 100%;
+  padding: 0 1rem;
+
+  border: solid 0.0625rem ${({ theme }) => theme.colors.behance_gray400};
+  border-radius: 0 0.3125rem 0.3125rem 0;
+  & > input {
+    width: 100%;
+    padding: 0 1rem;
+
+    border: none;
+
+    ${({ theme }) => theme.fonts.behance_acumin_pro_medium_17};
+  }
+`;
+const StButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 8rem;
+
+  border: none;
+  border-radius: 0.3125rem 0 0 0.3125rem;
+  background-color: ${({ theme }) => theme.colors.behance_blue};
+  color: ${({ theme }) => theme.colors.behance_white};
+  ${({ theme }) => theme.fonts.behance_acumin_pro_medium_17};
+
+  text-align: center;
+
+  & > span {
+    margin: 0.5rem 0rem;
+  }
 `;
