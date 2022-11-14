db592a1643bf1a15265841c7f7c18d994aa57b90
seohee0112 <ghdtj7@naver.com>
2022-11-14 20:55:27 +0900

    fix : 처음 화면 Home 으로 변경

---
+++ src/components/Home/Header.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+import { ICSearch } from '../../asset/icon';
+const Header = () => {
+  return <StHeaderWrapper></StHeaderWrapper>;
+};
+
+export default Header;
+const StHeaderWrapper = styled.header`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 100%;
+  height: rem;
+  padding: 0 1.875rem;
+`;
