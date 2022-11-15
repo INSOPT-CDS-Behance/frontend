568f88e9628792960af00aa4d3ba1256071d4bd7
seohee0112 <ghdtj7@naver.com>
2022-11-15 12:27:01 +0900

    style : header 에 필요한 svg img 저장 및 불러오기 구현

--- src/asset/icon/index.ts
+++ src/asset/icon/index.ts
@@ -1,9 +1,16 @@
 export { ReactComponent as ICBtnLiveContent } from './_btn_live contents.svg';
+export { ReactComponent as ICArrow } from './Arrow_down.svg';
 export { ReactComponent as ICBtnAdd } from './btn_add_big.svg';
+export { ReactComponent as ICBtnShare } from './btn_share.svg';
+export { ReactComponent as ICAdobe } from './ic_adobe.svg';
+export { ReactComponent as ICBehance } from './ic_behance.svg';
 export { ReactComponent as ICDropDown } from './ic_dropdown.svg';
 export { ReactComponent as ICFavorite } from './ic_favorite_fill_24.svg';
+export { ReactComponent as ICHomePreview } from './ic_home_preview.svg';
 export { ReactComponent as ICPerson } from './ic_person_fill_24.svg';
 export { ReactComponent as ICSearch } from './ic_search.svg';
 export { ReactComponent as ICStar } from './ic_star_fill_24.svg';
+export { ReactComponent as ICAlertFill } from './IcAlertFill.svg';
+export { ReactComponent as ICMailFill } from './IcMailFii.svg';
 export { ReactComponent as ICLineCol } from './Line 5.svg';
 export { ReactComponent as ICBtnNext } from './Next button.svg';
