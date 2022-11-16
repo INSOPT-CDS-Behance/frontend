48ccaf7306c52e9079224b1c96dc7d74c6f5bf9b
seohee0112 <ghdtj7@naver.com>
2022-11-16 09:10:33 +0900

    feat : home 화면에서 미리보기 content 불러오기 구현

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,25 @@
+import styled from 'styled-components';
+
+import { ContentPreview } from '../components/common';
 import { Header, MainHeader } from '../components/Home';
 const Home = () => {
   return (
     <div>
       <MainHeader />
       <Header />
+      <StContentSection>
+        {[1, 2, 3, 4, 5, 6, 6, 7, 7, 7, 7, 7, 7, 7, 2, 2, 2, , 2, , 2, 2, , 2].map((_, idx) => (
+          <ContentPreview key={idx} profileImg="" name="Wedge Studio" recommandCount={129} visibleCount={129} />
+        ))}
+      </StContentSection>
     </div>
   );
 };
 
 export default Home;
+
+const StContentSection = styled.section`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  margin: 0 1.875rem;
+`;
