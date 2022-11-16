e210ba929341d1f0c4c6467b94bd1a22d94be588
Seojisoo20191941 <amy0480@naver.com>
2022-11-16 16:10:42 +0900

    feat : 서치바 input 박스 완료

--- src/asset/icon/index.ts
+++ src/asset/icon/index.ts
@@ -3,5 +3,6 @@ export { ReactComponent as ArrowRight } from './arrowRight.svg';
 export { ReactComponent as GlassIcon } from './glass.svg';
 export { ReactComponent as HamburgerButton } from './hamburgerButton.svg';
 export { ReactComponent as ICAlertFill } from './IcAlertFill.svg';
+export { ReactComponent as ICDropdown } from './icDropdown.svg';
 export { ReactComponent as ICMailFill } from './IcMailFill.svg';
 export { ReactComponent as ICNoriLogo } from './noriLogoIcon.svg';
--- src/components/search/Searchbar.tsx
+++ src/components/search/Searchbar.tsx
@@ -1,15 +1,39 @@
 import { useState } from 'react';
 import styled from 'styled-components';
 
-import { GlassIcon } from '../../asset/icon';
+import { GlassIcon, ICDropdown } from '../../asset/icon';
+// import icColor from '../../asset/icon.icColor.svg';
+import icCreative from '../../asset/icon/icCreative.svg';
+import icLink from '../../asset/icon/icLink.svg';
+import icLocation from '../../asset/icon/icLocation.svg';
+import icPersonFill24 from '../../asset/icon/icPersonFill24.svg';
+import icSchool from '../../asset/icon/icShool.svg';
+import icTool from '../../asset/icon/icTool.svg';
+import theme from '../../styles/theme';
 
+interface Props {
+  isClicked: boolean;
+}
 const Searchbar = () => {
   const [clicked, setClicked] = useState<boolean[]>([true, false, false, false, false, false]);
   const categorys: string[] = ['프로젝트', '이미지', '프로토타입', '스트림', '인물', '무드보드'];
-  const indexs: boolean[] = [true, false, false, false, false, false];
-  interface Props {
-    isClicked: boolean;
+  const index: boolean[] = [true, false, false, false, false, false];
+
+  interface Arr {
+    id: number;
+    text: string;
+    src: string;
   }
+
+  const dropboxs: Arr[] = [
+    { id: 1, text: '크리에이티브 분야', src: icCreative },
+    { id: 2, text: '도구', src: icTool },
+    // { id: 3, text: '색상', src: icColor },
+    { id: 4, text: '위치', src: icLocation },
+    // { id: 5, text: '학교', src: icSchool },
+    { id: 6, text: '소스파일', src: icLink },
+    { id: 7, text: '구독', src: icPersonFill24 },
+  ];
 
   return (
     <>
@@ -21,23 +45,44 @@ const Searchbar = () => {
         <div>
           {categorys.map((category, index) => (
             // eslint-disable-next-line react/jsx-key, react/no-unknown-property
-            <button key={index} isClicked={clicked[index]}>
+            <CategoryButton key={index} isClicked={clicked[index]}>
               {category}
-            </button>
+            </CategoryButton>
           ))}
         </div>
       </InputContainer>
+      <DropBoxContainer>
+        {dropboxs.map((dropbox) => (
+          <button key={dropbox.id}>
+            <img src={dropbox.src} alt="드롭박스 아이콘" />
+            {dropbox.text}
+            <ICDropdown />
+          </button>
+        ))}
+      </DropBoxContainer>
     </>
   );
 };
 
 export default Searchbar;
 
+const DropBoxContainer = styled.section`
+  margin-top: 2.5rem;
+  margin-left: 1.875rem;
+  & > button {
+    margin-right: 1.25rem;
+    background-color: transparent;
+    border: 1px solid ${({ theme }) => theme.colors.behance_gray400};
+    border-radius: 0.3125rem;
+    ${({ theme }) => theme.fonts.behance_acumin_pro_regular_16};
+  }
+`;
+
 const Glass = styled(GlassIcon)`
   margin: 1rem 1.25rem 0 2.125rem;
 `;
 
-const InputContainer = styled.section<Props>`
+const InputContainer = styled.section`
   display: flex;
   justify-content: center;
   height: 4rem;
@@ -59,18 +104,6 @@ const InputContainer = styled.section<Props>`
     border-radius: 0 6.25rem 6.25rem 0;
     align-items: center;
     padding-left: 1.4375rem;
-    & > button {
-      padding: 0.625rem 0.75rem;
-      border: 0.0625rem solid transparent;
-      background-color: transparent;
-      ${({ theme }) => theme.fonts.behance_acumin_pro_medium_17};
-      margin-right: 1rem;
-      border-radius: 6.25rem;
-      height: 2.5rem;
-
-      background-color: ${(props) => props.isClicked && props.theme.colors.behance_black};
-      color: ${(props) => props.isClicked && props.theme.colors.behance_white};
-    }
   }
   & > div > input {
     ${({ theme }) => theme.fonts.behance_acumin_pro_medium_24};
@@ -82,4 +115,16 @@ const InputContainer = styled.section<Props>`
   & > div > input:focus {
     outline: none;
   }
+`;
+const CategoryButton = styled.button<Props>`
+  padding: 0.625rem 0.75rem;
+  border: 0.0625rem solid transparent;
+  background-color: transparent;
+  ${({ theme }) => theme.fonts.behance_acumin_pro_medium_17};
+  margin-right: 1rem;
+  border-radius: 6.25rem;
+  height: 2.5rem;
+
+  background-color: ${(props) => props.isClicked && props.theme.colors.behance_black};
+  color: ${(props) => props.isClicked && props.theme.colors.behance_white};
 `;
