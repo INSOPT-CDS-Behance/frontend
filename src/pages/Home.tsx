677b73acbd0e215b76c48fbaa9d24234cc029c99
seohee0112 <ghdtj7@naver.com>
2022-11-16 10:13:22 +0900

    feat : 헤더 상단에 고정 및 HeaderLayout 적용

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,34 @@
 import styled from 'styled-components';
 
-import { ContentPreview } from '../components/common';
-import { Header, MainHeader } from '../components/Home';
+import { ICBehance } from '../asset/icon';
+import { ContentPreview, PersonalInfo } from '../components/common';
+import { Header } from '../components/Home';
+import PageCategory from '../components/Home/PageCategory';
+import { HeaderLayout } from '../components/layout';
 const Home = () => {
   return (
-    <div>
-      <MainHeader />
-      <Header />
-      <StContentSection>
-        {[1, 2, 3, 4, 5, 6, 6, 7, 7, 7, 7, 7, 7, 7, 2, 2, 2, , 2, , 2, 2, , 2].map((_, idx) => (
-          <ContentPreview key={idx} profileImg="" name="Wedge Studio" recommandCount={129} visibleCount={129} />
-        ))}
-      </StContentSection>
-    </div>
+    <>
+      <HeaderLayout
+        isWhite={true}
+        Logo={<ICBehance />}
+        CategoryInfo={<PageCategory />}
+        PersonalInfo={<PersonalInfo />}></HeaderLayout>
+      <StBody>
+        <Header />
+        <StContentSection>
+          {[1, 2, 3, 4, 5, 6, 6, 7, 7, 7, 7, 7, 7, 7, 2, 2, 2, , 2, , 2, 2, , 2].map((_, idx) => (
+            <ContentPreview key={idx} profileImg="" name="Wedge Studio" recommandCount={129} visibleCount={129} />
+          ))}
+        </StContentSection>
+      </StBody>
+    </>
   );
 };
 
 export default Home;
-
+const StBody = styled.body`
+  padding-top: 4.375rem;
+`;
 const StContentSection = styled.section`
   display: grid;
   grid-template-columns: repeat(5, 1fr);
