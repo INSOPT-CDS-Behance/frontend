e440f2326b3938b7fc51acf3f1b003366f62ebd0
Seojisoo20191941 <amy0480@naver.com>
2022-11-18 06:46:02 +0900

    feat : 유사프로젝트 버튼에 연결

--- src/components/common/Hover.tsx
+++ src/components/common/Hover.tsx
@@ -1,30 +1,78 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 
-import { EyeOff, Folder, ICGlass, ThumbsUp } from '../../asset/icon';
+import { EyeOff, Folder, ICClose, ICDropdown, ICGlass, ICOpenLink, ThumbsUp } from '../../asset/icon';
+import ImgHomePreview from '../../asset/image/previewImg.png';
+import Thumbnail from '../search/Thumbnail';
 
 const Hover = () => {
+  const [projectClicked, setprojectClicked] = useState(false);
+
+  const handleProjectClick = () => {
+    setprojectClicked(true);
+  };
+  const handleXClick = () => {
+    setprojectClicked(false);
+  };
+
   return (
-    <Container>
-      <Button className="category">산업 디자인</Button>
-      <Button className="eyeOffButton">
-        <EyeOff />
-      </Button>
-      <p>KID CAM</p>
-      <footer>
-        <Button className="icGlass">
-          <Glass />
-          <p>유사프로젝트</p>
+    <>
+      {projectClicked && (
+        <StSimilarProjectWrapper>
+          <StLeft>
+            <div>
+              <p>다음과 유사</p>
+              <button type="button" onClick={handleXClick}>
+                <p>지우기</p>
+                <ICClose />
+              </button>
+            </div>
+            <img src={ImgHomePreview} alt="썸네일 이미지" />
+            <div>
+              <p>여러 소유자</p>
+              <ICDropdown fill="white" />
+            </div>
+            <div>
+              <ICOpenLink fill="${({ theme }) => theme.colors.behance_gray500}" />
+              <p>전체 프로젝트 보기</p>
+            </div>
+          </StLeft>
+          <hr />
+          <StRight>
+            <div></div>
+            {[1, 2, 3, 4, 5].map((_, idx) => (
+              <Thumbnail key={idx} profileImg="" name="Wedge Studio" recommandCount={129} visibleCount={129} />
+            ))}
+          </StRight>
+          <StHr>
+            <hr />
+            <hr />
+          </StHr>
+        </StSimilarProjectWrapper>
+      )}
+
+      <Container>
+        <Button className="category">산업 디자인</Button>
+        <Button className="eyeOffButton">
+          <EyeOff />
         </Button>
-        <Button className="folder">
-          <Folder />
-          <p>저장</p>
-        </Button>
-        <Button className="thumbsup">
-          <ThumbsUp />
-          <p>추천</p>
-        </Button>
-      </footer>
-    </Container>
+        <p>KID CAM</p>
+        <footer>
+          <Button className="icGlass" onClick={handleProjectClick}>
+            <Glass />
+            <p>유사프로젝트</p>
+          </Button>
+          <Button className="folder">
+            <Folder />
+            <p>저장</p>
+          </Button>
+          <Button className="thumbsup">
+            <ThumbsUp />
+            <p>추천</p>
+          </Button>
+        </footer>
+      </Container>
+    </>
   );
 };
 
@@ -107,4 +155,147 @@ const Button = styled.button`
   ${({ theme }) => theme.fonts.behance_acumin_pro_regular_12};
   color: ${({ theme }) => theme.colors.behance_white};
   border-radius: 1.25rem;
+`;
+
+const StSimilarProjectWrapper = styled.section`
+  display: flex;
+
+  position: absolute;
+  z-index: 3;
+
+  width: 120rem;
+  height: 35rem;
+  margin-left: -1.9rem;
+  margin-bottom: 35rem;
+
+  background-color: ${({ theme }) => theme.colors.behance_black};
+
+  & > hr {
+    width: 0.0625rem;
+    height: 25rem;
+
+    margin-top: 3rem;
+    margin-left: 2.3125rem;
+
+    flex-grow: 0;
+    transform: rotate(-180deg);
+
+    background-color: ${({ theme }) => theme.colors.behance_gray500};
+  }
+`;
+
+const StLeft = styled.article`
+  width: 18.75rem;
+  height: 25rem;
+  margin: 3rem 0 0 1.875rem;
+
+  color: ${({ theme }) => theme.colors.behance_white};
+
+  & > img {
+    width: 18.75rem;
+    height: 12.5rem;
+  }
+
+  & > div:nth-child(1) {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    margin-bottom: 1.5rem;
+
+    ${({ theme }) => theme.fonts.behance_acumin_pro_bold_17};
+
+    & > button {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      width: 6.125rem;
+      height: 2.25rem;
+
+      border: 1px solid transparent;
+      border-radius: 6.25rem;
+
+      ${({ theme }) => theme.fonts.behance_acumin_pro_bold_14};
+      color: ${({ theme }) => theme.colors.behance_white};
+      background-color: ${({ theme }) => theme.colors.behance_gray700};
+
+      & > p {
+        margin-right: 0.5rem;
+      }
+    }
+  }
+
+  & > div:nth-child(3) {
+    display: flex;
+    align-items: center;
+
+    margin-top: 1.25rem;
+    ${({ theme }) => theme.fonts.behance_acumin_pro_bold_17};
+    & > p {
+      margin-right: 0.25rem;
+    }
+  }
+  & > div:nth-child(4) {
+    display: flex;
+    align-items: center;
+
+    margin-top: 5.1875rem;
+
+    ${({ theme }) => theme.fonts.behance_acumin_pro_bold_14};
+
+    & > p {
+      margin-left: 0.5rem;
+      color: ${({ theme }) => theme.colors.behance_gray500};
+    }
+  }
+`;
+
+const StRight = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+
+  margin: -9.625rem 0 0 1.5rem;
+  ${({ theme }) => theme.fonts.behance_acumin_pro_bold_16};
+  color: ${({ theme }) => theme.colors.behance_white};
+
+  & > div {
+    position: absolute;
+    z-index: 3;
+    float: right;
+
+    width: 9.375rem;
+    height: 24.25rem;
+    margin-top: 10rem;
+    margin-left: 87.625rem;
+
+    background-image: linear-gradient(to left, black, transparent);
+  }
+`;
+
+const StHr = styled.section`
+  display: flex;
+  justify-content: flex-start;
+
+  margin-top: 32.875rem;
+
+  position: absolute;
+  z-index: 2;
+
+  & > hr:nth-child(1) {
+    border: 1px solid;
+    background-color: ${({ theme }) => theme.colors.behance_blue};
+    position: absolute;
+    z-index: 2;
+    width: 30rem;
+    height: 0.125rem;
+    margin: 0;
+  }
+  & > hr:nth-child(2) {
+    border: 1px solid;
+    background-color: ${({ theme }) => theme.colors.behance_white};
+    width: 120rem;
+    height: 0.125rem;
+    margin: 0;
+  }
 `;
