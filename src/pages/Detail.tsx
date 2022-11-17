30dbd83cb190098f65f7c70a2f92ba1f70f229f9
Seojisoo20191941 <amy0480@naver.com>
2022-11-18 08:09:19 +0900

    feat : 이미지 삽입

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,16 +1,26 @@
 import styled from 'styled-components';
 
+import { ImgScroll } from '../asset/image';
 import DetailBlackHeader from '../components/Detail/DetailBlackHeader';
 import DetailWhiteHeader from '../components/Detail/DetailWhiteHeader';
 
 const Detail = () => {
   return (
-    <StDetailWrapper>
+    <>
       <DetailBlackHeader />
       <DetailWhiteHeader />
-
-    </StDetailWrapper>
+      <StDetailWrapper>
+        <ImgScroll />
+      </StDetailWrapper>
+    </>
   );
 };
 
 export default Detail;
+
+const StDetailWrapper = styled.section`
+  display: flex;
+  justify-content: center;
+
+  background-color: ${({ theme }) => theme.colors.behance_black};
+`;
