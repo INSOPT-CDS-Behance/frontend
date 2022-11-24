c0581b7f452012a7108f35d61ddf326369c3dd4d
seobbang <seobbangway@naver.com>
2022-11-24 13:58:58 +0900

    feat : API 통신 'reqAPI'로 수정

--- src/utils/lib/moodboard.ts
+++ src/utils/lib/moodboard.ts
@@ -1,6 +1,4 @@
 /* eslint-disable simple-import-sort/imports */
-import axios from 'axios';
-
 import { EditMoodBoard } from '../../types/mypage';
 
 import { reqAPI } from '.';
@@ -8,7 +6,6 @@ import { reqAPI } from '.';
 export const getMoodBoard = async () => {
   return reqAPI.get(`/moodboard`);
 };
-
 export const patchMoodBoard = async (ChangedDataBody: EditMoodBoard) => {
-  return axios.patch(`${process.env.REACT_APP_IP}moodboard`, ChangedDataBody);
+  return reqAPI.patch(`/moodboard`, ChangedDataBody);
 };
